7af06e33fa779d570f4cfef786dde99c1e1c6597
Summer Li <summer.li@pwc.com>
2018-02-09 17:15:53 +0800

    ajax in progress

--- src/hello.js
+++ src/hello.js
@@ -1,4 +1,4 @@
-class Hello {
+class Refresh {
 	constructor(config) {
 		this.target = config.target;
 	}
@@ -8,9 +8,30 @@ class Hello {
 		`;
 	}
 
+	doRefresh() {
+		
+		document.getElementById('refresh').addEventListner('click', makeRequest);
+	}
+
+	makeRequest() {
+		var httpRequest = new XMLHttpRequest();
+		if (!httpRequest) {
+			alert('Giving up');
+			return false;
+		}
+		httpRequest.onreadystatechange = alertContents;
+		httpRequest.open('GET', 'test.html');
+		httpRequest.send();
+	}
+
+	alertContents() {
+		
+	}
+
+
 	ten() {
 		return 10;
 	}
 }
 
-export default Hello;
+export default Refresh;
